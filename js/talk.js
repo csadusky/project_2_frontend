e4089bae8223be51becb21fd920270777e6e7e45
courtneysadusky <courtneysadusky@gmail.com>
2015-06-04 18:44:48 -0400

    able to post comments

--- js/talk.js
+++ js/talk.js
@@ -1,66 +1,110 @@
 $(document).ready(function(){
 
+  // Pre-load list of lines
   $.ajax({
     type: 'GET',
     url: "http://localhost:3000/lines"
   }).done(function(line_data){
-    console.log(line_data);
+    // console.log(line_data);
     line_data.forEach(function(line){
       // $("#lines").append("<li id='" + line.id + "'>" + line.color + "</li>");
       $("#lines").append("<button id='" + line.id + "'>" + line.color + "</button>");
     })
-  })
-  .fail(function(line_data){
+  }).fail(function(line_data){
     console.log("failed when going to get line data");
     alert("failed");
   })
 
-  $('#lines').on("click",function(event){
-    alert("clicked" + event.target.id);
+  var renderLine = function(lineID){
     $.ajax({
       type: 'GET',
-      url: "http://localhost:3000/lines/" + event.target.id
+      url: "http://localhost:3000/lines/" + lineID
     })
     .done(function(line_data){
-      console.log(line_data);
-      var html = "<dl id='current_line' data-current-line='" + line_data.id + "'><dt>Color</dt><dd>" + line_data.color + "</dd><dt>Direction</dt><dd>" + line_data.direction + "</dd></dl>";
+      // console.log(line_data);
+      var lineHTML = "<dl id='current_line' data-current-line='" + line_data.id + "'><dt>Color</dt><dd>" + line_data.color + "</dd><dt>Direction</dt><dd>" + line_data.direction + "</dd></dl>";
 
 
       $('#line').html("");
-      $('#line').append(html);
+      $('#line').append(lineHTML);
 
       line_data.comments.forEach(function(comment){
         var commentHTML = "<p><h3>" + comment.user.username + "</h3>" + comment.post + " " + comment.time +  "</p>";
         $('#line').append(commentHTML);
       })
-
     })
     .fail(function(line_data){
       console.log("failed");
     });
+  }
+
+  // List color, direction, and comments for the clicked line.
+  $('#lines').on("click",function(event){
+    // alert("clicked element with id " + event.target.id);
+    renderLine(event.target.id);
   });
 
-  $('#submit-button').on("click",function(event){
-    var commentData ={comment: {
-      post: $("#new-post").val()
-    }}
-
+  // For submitting comments
+  $('#submit-button').on("click",function(){
+    // Get the ID of the current line from our HTML (which has already loaded it).
     var currentLineId = $('#current_line').data('current-line');
-    console.log('created post for user with line id of' + currentLineId);
-      $.ajax({
-        type: 'POST',
-        url:"http://localhost:3000/lines/" + currentLineId + "/comments",
-        dataType: "json",
-        data: commentData
-      })
-      .done(function(){
-        alert("success");
-      })
-      .fail(function(){
-        alert("failure");
-      })
-      $('#line').append(commentHTML);
-    });
+
+    // Create a comment to add to the current line.
+    var commentData ={comment: {
+      post: $("#new-post").val(),
+      line_id: currentLineId,
+      user_id: 1
+    }};
+
+    // POST the comment to the API.
+    //console.log('created post for user with line id of' + currentLineId);
+    $.ajax({
+      type: 'POST',
+      url:"http://localhost:3000/lines/" + currentLineId + "/comments",
+      dataType: "json",
+      data: commentData
+    })
+    .done(function(){
+      // alert("success");
+      renderLine(currentLineId);
+    })
+    .fail(function(){
+      alert("failure");
+    })
+  });
+
+  // For creating users
+  $('#user-button').on("click",function(){
+
+    //1. Create a new User
+
+
+    //2. POST the new User to our API.
+    //3. (Optional) Render data about the User
+
+    //
+      // var userData ={user: {
+      //   post: $("#new-username").val()
+      // }}
+
+      // var newUserId = $('#new-user').data;
+      // console.log('created new user');
+
+      // $.ajax({
+      //   type: 'POST',
+      //   url:"http://localhost:3000/",
+      //   dataType: "json",
+      //   data:
+      // })
+      // .done(function(){
+      //   alert("success");
+      // })
+      // .fail(function(){
+      //   alert("failure");
+      // })
+    //
+  });
+
 });
 
 
